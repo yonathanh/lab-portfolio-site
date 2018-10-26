ed485ad7a1d761d75c1c4e0d88550ef15df8dfad
Jonathan Harris <yochananh@gmail.com>
2018-10-26 15:23:09 -0400

    npm run build

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./components/App/App";
 import registerServiceWorker from "./registerServiceWorker";
 import { BrowserRouter as Router } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.css";
+//import "bootstrap/dist/css/bootstrap.css";
 
 ReactDOM.render(
   <Router>
